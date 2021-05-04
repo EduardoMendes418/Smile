c08d33505b7831016de4b61b33fce2f4e435b76f
unknown <eduardo.mendes@dev008.ddtotalcps.com.br>
2021-05-04 08:46:55 -0300

    first commit

--- frontend/src/pages/AdPage/index.js
+++ frontend/src/pages/AdPage/index.js
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
 import useApi from '../../helpers/Api';
-import { PageArea }  from './styled';
+import { PageArea, Fake }  from './styled';
 import { useParams } from 'react-router-dom';
 
 import { PageContainer } from '../../components/templatecomponents';
+import ReactDOM from "react-dom";
 
 
 const  Page = () => {
@@ -11,31 +12,37 @@ const  Page = () => {
     const api = useApi();
     const { id } = useParams();
 
-    const  [ locading, setLoading ] = useState(true);
+    const  [ loading, setLoading ] = useState(true);
     const  [adInfo , setAdInfo ] = useState([]);
 
     //Pagina Fake
     return (
+        
         <PageContainer>
             <PageArea>
                 <div className="leftSide">
                     <div className="box">
                         <div className="adImage">
-                            ...
+                            {loading && <Fake height={300}/>}
                         </div>
                         <div className="adInfo">
                             <div className="adName">
-                               ...
+                                {loading && <Fake height={20}/>}
                             </div>
                             <div className="adDescrition">
-                                ...
+                                {loading && <Fake height={100}/>}
                             </div>
                         </div>
                     </div>
                 </div>
+
                 <div className="rightSide">
-                    <div className="box"> ... </div>
-                    <div className="box"> ... </div>
+                    <div className="box box--padding">
+                        {loading && <Fake height={20}/>}
+                    </div>
+                    <div className="box box--padding">
+                        {loading && <Fake height={20}/>}
+                    </div>
                 </div>
             </PageArea>
         </PageContainer>
--- frontend/src/pages/AdPage/styled.js
+++ frontend/src/pages/AdPage/styled.js
@@ -1,38 +1,41 @@
 import styled from 'styled-components';
 
 
-export const FakeNew = styled.div`
-
+export const Fake = styled.div`
+     background-color: #DDD;
+     height:${props=>props.height || 20}px;
 `;
 
 export const PageArea = styled.div`
-
     display:flex;
     margin-top:20px;
 
     .box {
-        background-color:#fff;
+        background-color: #FFF;
         border-radius:5px;
-        box-shadow:0px 0px 0px 2px #ddd;
-        margin-bottom:20px;
+        box-shadow:0px 0px 0px 3px #999;
+        margin-bottom: 20px;
     }
 
-    .leftSide{
+    .box--padding{
+        padding: 10px;
+    }
+
+    .leftSide {
         flex:1;
         margin-right:20px;
 
-
-        .adImage{
+        .adImage {
 
         }
 
         .adInfo{
+            padding: 10px;
 
             .adName{
-
+                margin-bottom:20px;
             }
-
-            .adDescription{
+            .adDescription {
 
             }
 
@@ -40,7 +43,7 @@ export const PageArea = styled.div`
 
     }
 
-    .rightSide{
+    .rightSide {
         width: 250px;
     }
 
