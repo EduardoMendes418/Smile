01aabce22bdad9200da70a13ce695f0c6166bc4a
unknown <eduardo.mendes@dev008.ddtotalcps.com.br>
2021-05-11 08:29:29 -0300

    first commit

--- frontend/src/pages/Ads/index.js
+++ frontend/src/pages/Ads/index.js
@@ -1,9 +1,9 @@
 import React, { useState , useEffect } from 'react';
-import { PageArea, SeachArea } from './styled';
+import { PageArea } from './styled';
 import { PageContainer } from '../../components/templatecomponents';
 import AdItem from '../../components/partials/AdItem';
 
-import { Link } from 'react-router-dom';
+import { useHistory, useLocation } from 'react-router-dom';
 
 //buscando api no serve hook
 import useApi from '../../helpers/Api';
@@ -11,11 +11,46 @@ import useApi from '../../helpers/Api';
 const  Page =  () => {
     //chamando a api
     const api = useApi();
+    const history = useHistory();
+
+    //buscar o parametro  exemplo:cat
+    const useQueryString = () =>{
+        return new URLSearchParams(useLocation().search);
+    }
+    const query = useQueryString();
+
+    //state puxando a informação    
+    const [q, setQ] = useState(query.get('q') != null ? query.get('q') : '');
+    const [cat, setCat] = useState(query.get('cat') != null ? query.get('cat') : '');
+    const [state, setState] = useState(query.get('state') != null ? query.get('state') : '');
+
 
     const [stateList, setStateList] = useState([]);
     const [categories,setCategories] = useState([]);
     const[adList, setAdList] = useState([]);
-    
+
+
+    //MONITORA cat/q/state
+    useEffect(()=> {
+        //codicao de verificao de states
+        let queryString = [];
+        if(q){
+            queryString.push(`q=${q}`);
+        }
+        if(cat){
+            queryString.push(`cat=${cat}`);
+        }
+        if(state){
+            queryString.push(`state=${state}`);
+        }
+
+        //junta todos com join  e separa & comercial
+        history.replace({
+            search:`?${queryString.join('&')}`
+        });
+    },[q,cat,state]);
+
+
     //listade estados
     useEffect(() =>{
         const getStates = async () => {
@@ -48,45 +83,59 @@ const  Page =  () => {
     
 
     return (
-           <>
-            <SeachArea>
-                <PageContainer>
-                    <div className="searchBox">
-                        <form method="GET" action="/ads">
-                            <input type="text" name="q" placeholder="O que voce procura ?" />
-                            <select name="state">
-                                {stateList.map((i,k)=>
-                                    <option key={k} value={i.name}>{i.name}</option>
-                                )}
-                            </select>
-                            <button>Pesquisar</button>
-                        </form>
-                    </div>
-                    <div className="categoryList">
-                        {categories.map((i,k)=>
-                            <Link key={k} to={`/ads?cat=${i.slug}`} className="categoryItem">
-                                <img src={i.img} alt="" />
-                                <span>{i.name}</span>
-                            </Link>
-                        )}
-                    </div>
-                </PageContainer>
-            </SeachArea>
+        <PageContainer>
+             <PageArea>
+                <div className="leftSide">
+                    <form method="GET">
+                        
+                        <input 
+                         type="text" 
+                         name="q" 
+                         placeholder="O que você procura?" 
+                         value={q}
+                         onChange={e=>setQ(e.target.value)}
 
-            <PageContainer>
-                <PageArea>
-                    <h2>Anúncios Recentes</h2>
-                    <div className="list">
-                        {adList.map((i,k)=>
-                            <AdItem key={k} data={i}/>
-                        )}
-                    </div>
-                    <Link to="/ads" className="seeAllLink">Ver todos</Link>
-                    <hr/>
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
-                </PageArea>
-            </PageContainer>
-           </> 
+                         />
+                        
+                        {/*LISTA ESTADOS */}
+                        <div className="filterName">Estado: </div>
+                        <select 
+                        name="state" 
+                        value={state}
+                        onChange={e=>setState(e.target.value)}    
+                        >
+                            <option></option> 
+                               {stateList.map((i,k) =>
+                                    <option key={k} value={i.name}>{i.name}</option>
+                               )}
+                        </select>
+
+                        {/*LISTA CATEGORIA*/}       
+                        <div className="filterName">Categoria:</div>
+                        <ul>
+        
+                            {categories.map((i,k) => 
+
+                                //CATEGORIAS ASSIMQ BUSCAR ATIVADAS/ITEM
+                                <li 
+                                key={k} 
+                                className={cat==i.slug?'categoryItem active':'categoryItem'}
+                                onClick={()=>setCat(i.slug)}
+                                >
+                                      <img src={i.img} alt="" /> 
+                                      <span>{i.name}</span> 
+                                </li>
+                            )}
+
+                        </ul>
+                    </form>
+                </div>
+                <div className="rightSide">
+                    ...
+                </div>
+             </PageArea>
+        </PageContainer>
+       
     );
 }
 
--- frontend/src/pages/Ads/styled.js
+++ frontend/src/pages/Ads/styled.js
@@ -1,109 +1,71 @@
 import styled from 'styled-components';
 
 
-export const SeachArea = styled.div`
-    background-color: #DDD;
-    border-bottom: 1px solid #CCC;
-    padding: 20px 0;
-
-    .searchBox{
-        background-color: #ffc70b;
-        padding:20px 15px;
-        border-radius:5px;
-        box-shadow:1px 1px 1px 0.3 rgb(0,0,0,0,0.2);
-        display:flex;
-
-        form {
-            flex:1;
-            display:flex;
-
-            input , select {
-                height:40px;
-                border:0;
-                border-radius: 5px;
-                outline:0;
-                font-size:15px;
-                color:#000;
-                margin-right:20px;
-                font-family: 'Open Sans', sans-serif;
-            }    
-
-            input{
-                flex:1 !important;
-                padding-left:0 10px;
-               
-            }
-
-            select{
-                width: 100px; 
-            }
-
-            button{
-                background-color: #f4f4f4;
-                font-size:15px;
-                border:0;
-                border-radius:5px;
-                color: #000;
-                height: 40px;
-                padding: 0 20px;
-                cursor:pointer;
-                font-family: 'Open Sans', sans-serif;
-                font-weight: 700;
-            }
-        }
-    }
-
-    .categoryList{
-        display:flex;
-        flex-wrap:wrap;
-        margin-top:20px;
-
-        .categoryItem{
-            width:25%;
-            display:flex;
-            align-items:center;
-            color:#000;
-            text-decoration:none;
-            height:50px;
-            margin-bottom:10px;
-            font-size:18px;
-            font-weight:bold;
-
-            &:hover{
-                color:#9999;
-            }
-
-            img {
-                width: 45px;
-                height: 45px;
-                margin-right: 10px;
-            }
-        }
-    }
-`;
-
 export const PageArea = styled.div`
+display:flex;
+margin-top:20px;
 
-    h2{
-        font-size: 20px
+.leftSide{
+    width: 250px;
+    margin-right:10px;
+
+
+    .filterName{
+        font-size:15px;
+        margin:10px 0;
     }
 
-    .list{
-        display:flex !important;
-        flex-wrap:wrap !important;
+    input , select {
+        width:100%;
+        height:40px;
+        border:2px solid #ffc70b;
+        border-radius:5px;
+        outline:0;
+        font-size:15px;
+        color:#000;
+        padding:10px;
+    }
 
-        .aditem{
-            width: 25%;
+    ul, li {
+        margin:0;
+        padding:0;
+        list-style:none;
+    }
+
+    .categoryItem{
+        display:flex;
+        align-items:center;
+        padding:10px;
+        border-radius:5px;
+        color:#000;
+        cursor:pointer;
+
+        img{
+            width:25px;
+            height:25px;
+            margin-right:5px;
+        }
+
+        span{
+            font-size:14px;
         }
 
     }
-    
-    .seeAllLink{
+
+    .categoryItem:hover,
+    .categoryItem.active{
+        background-color: #ffc70b;
         color:#000;
-        text-decoration:none;
-        font-weight:bold;
-        display:inline-block;
-        margin-top: 10px;
     }
+
+
+
+
+}
+
+.rightSide{
+    flex:1;
+}
+
 
 `;
